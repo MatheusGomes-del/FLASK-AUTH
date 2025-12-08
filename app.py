0d371aec69307ca29b148d7ab5a9351c74a61a11
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2025-12-07 21:52:14 -0300

    feat: implementa rota de leitura de usuário atual e update na senha do usuário logado

--- app.py
+++ app.py
@@ -63,6 +63,33 @@ def create_user():
     return jsonify({"message": "Dados inválidos"}), 401
 
 
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return {"username": user.username}
+
+    return jsonify({"message": "Usuário não encontrado"}), 404
+
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_passord_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+
+        return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+    
+    return jsonify({"message": "Usuário não encontrado"}), 404
+
+
+
 @app.route("/", methods=['GET'])
 def hello_world():
     return "hello world"
