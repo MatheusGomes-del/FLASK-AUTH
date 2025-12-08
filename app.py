29552ce0d2b9d3b5ebbc140807f5a827576f054a
MatheusGomes-del <matheus.gomes.santos@outlook.com>
2025-12-07 21:31:43 -0300

    feat: implementa registro livre de usuário

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from dotenv import load_dotenv
 import os
 
@@ -40,6 +40,27 @@ def login():
     
     return jsonify({"message": "Credenciais inválidas"}), 400
 
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+       user = User(username=username, password=password)
+       db.session.add(user)
+       db.session.commit()
+       return jsonify({"message": "Usuário cadastrado com sucesso"}) 
+
+    return jsonify({"message": "Dados inválidos"}), 401
 
 
 @app.route("/", methods=['GET'])
